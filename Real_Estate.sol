d7971852a0169ddef40e3e890ed5861e7432932d
Ajeet Ram Verma <86039147+ajeetram@users.noreply.github.com>
2023-02-06 23:25:11 +0530

    first commit

---
+++ Real_Estate.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity^0.8.18;
+
+import "https://github.com/Openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+contract RealEstate
+{
+    using SafeMath for uint256;
+    
+    struct Property
+    {
+        uint256 price;
+        address owner;
+        bool forSale;
+        string Pro_name;
+        string pro_description;
+        string location;
+    }
+
+    mapping(uint256=>Property) public properties;
+
+    uint256[] public property_Ids;
+
+    event Property_Sold(uint256 property_Ids);
+
+    function listPropertyForSale(uint256 _property_Ids, uint256 _price, string memory _name, string memory _description, string memory _location)
+    public 
+    {
+        Property memory newProperty = Property({price:_price, owner:msg.sender,forSale:true,name:_name, _description:_description});
+    
+
+    properties[_property_Ids] = newProperty;
+    property_Ids.push(_property_Ids);
+    }
+
+    function buyProperty(uint256 _property_Ids) public payable
+    {
+        Property storage property = properties[_property_Ids];
+
+        require(property.forSale,"Propery is no for sale");
+        require(property.price<=msg.value,"Insuffiecient funds");
+
+        property.owner = msg.sender;
+        property.forSale = false;
+         payable(Property.owner).transfer(property.price);
+
+         emit Property_Sold(_property_Ids);
+    } 
+
+}
